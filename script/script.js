291810dbefd3184beef7f22e880d2fa91f86fc67
princetonjeffries <71167628+princetonjeffries@users.noreply.github.com>
2021-03-12 16:31:33 -0700

    page transitions and spinning button

--- script/script.js
+++ script/script.js
@@ -3,9 +3,14 @@ $(function(){
     $('#arrow').on('click', function(){
         $('.box1').fadeTo(1000, 0);
         $('.box2').fadeTo(1000,1);
+        //$(this).addClass('spinning');
+        $(this).addClass('spinning').delay(2800).queue(function( next ){
+            $(this).removeClass('spinning'); 
+            next();
+        });
         i++;
         if(i == 2){
-            $('.box2').fadeTo(1000, 0);
+            $('.box2').fadeTo(1000,0);
             $('.box3').fadeTo(1000,1);
         }
         if(i == 3){
